1fc0fa5180727c8f71b7bf9f10e0c31d683fe85d
CyberButton <87766623307Kk>
2023-11-22 00:28:00 +0600

    upd

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mysite.dbas2 import views
+from dbas2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
